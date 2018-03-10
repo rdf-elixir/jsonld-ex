688731cdb03d086cdadf46e97054539152cdc630
Marcel Otto <marcelotto@gmx.de>
2018-03-10 01:12:28 +0100

    Revert "Add remote context processing (#2) [WIP]"
    This reverts commit aac4127aeb449cbbee84d36cfce9155f6f4d4bab.

--- lib/json/ld/document_loader/remote_document.ex
+++
@@ -1,9 +0,0 @@
-defmodule JSON.LD.DocumentLoader.RemoteDocument do
-  @type t :: %JSON.LD.DocumentLoader.RemoteDocument{context_url: String.t,
-    document_url: String.t,
-    document: any}
-
-  defstruct context_url: nil,
-    document_url: nil,
-    document: nil
-end
