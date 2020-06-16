1d00bc6a40bca5f64d214e8c6139e3c205f243c4
Marcel Otto <marcelotto@gmx.de>
2020-06-16 22:56:13 +0200

    Enable JSON-LD remote-doc test suite

---
+++ test/suite/remote_doc_test.exs
@@ -0,0 +1,20 @@
+defmodule JSON.LD.TestSuite.RemoteDocTest do
+  use ExUnit.Case, async: false
+
+  import JSON.LD.TestSuite
+
+  setup_all do
+    [base_iri: manifest("remote-doc")["baseIri"]]
+  end
+
+  test_cases("remote-doc")
+  |> Enum.each(fn %{"name" => name, "input" => input} = test_case ->
+      @tag :test_suite
+      @tag :remote_doc_test_suite
+      @tag data: test_case
+      test "#{input}: #{name}",
+          %{data: %{"input" => input, "expect" => output} = test_case, base_iri: base_iri} do
+        assert JSON.LD.expand(j(input), test_case_options(test_case, base_iri)) == j(output)
+      end
+    end)
+end
