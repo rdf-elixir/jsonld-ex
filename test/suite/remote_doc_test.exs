c149f860158bbf5336216a824451fdd8229e256a
rustra <rustra@disroot.org>
2020-06-17 22:42:28 +0200

    Improve RemoteDocTest

--- test/suite/remote_doc_test.exs
+++ test/suite/remote_doc_test.exs
@@ -9,20 +9,29 @@ defmodule JSON.LD.TestSuite.RemoteDocTest do
 
   test_cases("remote-doc")
   |> Enum.each(fn %{"name" => name, "input" => input} = test_case ->
-      @tag :test_suite
-      @tag :remote_doc_test_suite
-      @tag data: test_case
-      case hd(test_case["@type"]) do
-        "jld:PositiveEvaluationTest" ->
-          test "#{input}: #{name}",
-               %{data: %{"input" => input, "expect" => output} = test_case, base_iri: base_iri} do
-            assert JSON.LD.expand(j(input), test_case_options(test_case, base_iri)) == j(output)
-          end
-        "jld:NegativeEvaluationTest" ->
-          @tag skip: "TODO: "
-          test "#{input}: #{name}",
-               %{data: %{"input" => input, "expect" => output} = test_case, base_iri: base_iri} do
-          end
-      end
-    end)
+    if input in ~w[remote-doc-0005-in.jsonld remote-doc-0006-in.jsonld remote-doc-0007-in.jsonld] do
+      @tag skip: "TODO: Missed test file"
+    end
+
+    if input in ~w[remote-doc-0010-in.json remote-doc-0011-in.jldt] do
+      @tag skip: "TODO: Context from Link header is unsupported"
+    end
+
+    @tag :test_suite
+    @tag :remote_doc_test_suite
+    @tag data: test_case
+    case hd(test_case["@type"]) do
+      "jld:PositiveEvaluationTest" ->
+        test "#{input}: #{name}",
+             %{data: %{"input" => input, "expect" => output} = test_case, base_iri: base_iri} do
+          assert JSON.LD.expand(j(input), test_case_options(test_case, base_iri)) == j(output)
+        end
+
+      "jld:NegativeEvaluationTest" ->
+        @tag skip: "TODO: "
+        test "#{input}: #{name}",
+             %{data: %{"input" => input, "expect" => error} = test_case, base_iri: base_iri} do
+        end
+    end
+  end)
 end
