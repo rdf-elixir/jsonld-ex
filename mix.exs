f4bd412968c18688f78cf121347ce42a34987a7c
Marcel Otto <marcelotto@gmx.de>
2021-12-13 13:28:28 +0100

    Drop support for Elixir versions < 1.10

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule JSON.LD.Mixfile do
     [
       app: :json_ld,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
