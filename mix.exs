ff15447ce516e4bac7071a4980bafd79c8a7fcb1
Marcel Otto <marcelotto@gmx.de>
2025-04-06 17:12:44 +0200

    Drop support for Elixir versions < 1.15
    Elixir v1.15 fixes some problems with URI.merge/2

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule JSON.LD.Mixfile do
     [
       app: :json_ld,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
