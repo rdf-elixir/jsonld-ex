6a14e634f4f8c464a2dac1ca4600de372d08b3db
Marcel Otto <marcelotto@gmx.de>
2025-04-09 18:01:43 +0200

    Add opt-in comment to run W3C test suite remotely

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,6 @@
 import Config
 
 config :json_ld,
-  max_contexts_loaded: 5,
-  w3c_test_suite_run_mode: :local
+  # comment this in to run tests against the test files hosted on GitHub (takes much longer and fails occasionally)
+  # w3c_test_suite_run_mode: :remote,
+  max_contexts_loaded: 5
